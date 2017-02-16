78e339f6d9eb5f829398b0749ffddd2b0ada25aa
Honiix <laurent.indermuehle@gmail.com>
2017-02-16 22:58:27 +0100

    Now reads the savegame from clipboard

--- savereader.py
+++ savereader.py
@@ -1,14 +1,12 @@
 import base64
 import json
+import pyperclip
 
+raw = pyperclip.paste()
 splitter = "Fe12NAfA3R6z4k0z"
-
-with open('savegame.txt', 'r') as sg:
-    raw = sg.read()
-
 save = raw.split(splitter)[0]
 
 save_json_base64 = ''.join([char for char in save[::2]])
-save_json = base64.b64decode(save_json_base64)
+save_json = base64.b64decode(save_json_base64).decode('UTF-8')
 
 print(json.dumps(json.loads(save_json), sort_keys=True, indent=4))
